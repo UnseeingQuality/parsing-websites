34e5e9a0fef2234a09f1272015cf7d09d1b93449
UnseeingQuality <balakinaa2016@mail.ru>
2023-06-27 00:48:31 +0500

    shaman

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ import os
 
 
 def main():
-    uvicorn.run("app.app:app", host='0.0.0.0', port=4567, reload=True, log_level='info', workers=os.cpu_count())
+    uvicorn.run("app.app:app", host='127.0.0.1', port=4567, reload=True, log_level='info', workers=os.cpu_count())
 
 
 if __name__ == "__main__":
--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from .routers import routers
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
 origins = [
-    "http://localhost:3000/",
+    "http://127.0.0.1:3000",
 ]
 
 app.add_middleware(
@@ -16,6 +16,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def route_preflight(request: Request, response: Response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
 
 for router in routers:
     app.include_router(router)
